f2e7f3a692ebd48aec914bcda1793b0bd5ee1982
Alexis D'Ambrosio <alexis.dambrosio@edu.devinci.fr>
2021-09-22 11:33:48 +0200

    passage en anglais

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 btn.onclick = () => {
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + champ.value + "&lang=fr&units=metric&appid=be2412c99a491fffa5e50a5236834757")
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + champ.value + "&lang=en&units=metric&appid=be2412c99a491fffa5e50a5236834757")
   .then(reponse => reponse.json())
   .then(data => {
-    temp.textContent = "Température : " + Math.round(data.main.temp) + " °C";
-    humidity.textContent = "Humidité : " + data.main.humidity + " %";
+    temp.textContent = "Temperature : " + Math.round(data.main.temp) + " °C";
+    humidity.textContent = "Humidity : " + data.main.humidity + " %";
     description.textContent = data.weather[0].description;
     console.log(data);
     console.log(data.weather[0].icon);
