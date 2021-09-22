178f2ecd0eae1155f69b9c77c1585ec6df421b1b
Xavier Lami <xavier.lami@edu.devinci.fr>
2021-09-22 09:27:50 +0200

    last upt

---
+++ script.js
@@ -0,0 +1,9 @@
+btn.onclick = () => {
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + champ.value + "&lang=fr&units=metric&appid=be2412c99a491fffa5e50a5236834757")
+  .then(reponse => reponse.json())
+  .then(data => {
+    temp.textContent = "Température : " + Math.round(data.main.temp) + " °C";
+    humidity.textContent = "Humidité : " + data.main.humidity + " %";
+    console.log(data);
+  })
+}
