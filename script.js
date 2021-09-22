9a777fee81e47bf1887fe47d682ce55d347e8971
Xavier Lami <xavier.lami@edu.devinci.fr>
2021-09-22 09:43:30 +0200

    add index & js functionnal

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ btn.onclick = () => {
   .then(data => {
     temp.textContent = "Température : " + Math.round(data.main.temp) + " °C";
     humidity.textContent = "Humidité : " + data.main.humidity + " %";
+    description.textContent = "Description : " + data.weather[0].description;
     console.log(data);
   })
 }
