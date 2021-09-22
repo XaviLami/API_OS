0a47ad7587627775e9d9b6557600bc20d9f74a17
Alexis D'Ambrosio <alexis.dambrosio@edu.devinci.fr>
2021-09-22 12:31:44 +0200

    correction

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+var apiKey = "be2412c99a491fffa5e50a5236834757"; //Enter your Api key
 
 champ.onchange = () => {
-  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + champ.value + "&lang=en&units=metric&appid=be2412c99a491fffa5e50a5236834757")
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=" + champ.value + "&lang=en&units=metric&appid=" + apiKey)
   .then(reponse => reponse.json())
   .then(data => {
     temp.textContent = "Temperature : " + Math.round(data.main.temp) + " °C";
@@ -10,10 +11,11 @@ champ.onchange = () => {
     console.log(data.weather[0].icon);
     document.getElementById("myImg").src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
     document.getElementById("error").style.display = "none";
-    document.getElementById("none").style.display = "block"
+    document.getElementById("none").style.display = "block";
   }).catch(function() {
     console.log("error");
     document.getElementById("error").textContent = "This city does not exist";
-    document.getElementById("none").style.display = "none"
+    document.getElementById("error").style.display = "block";
+    document.getElementById("none").style.display = "none";
 });
 }
